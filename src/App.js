81e7fca211fb09fc984a02d44dd29dcef352008f
Israel Boquin Cancino <ip345622@gmail.com>
2023-09-21 14:39:47 -0500

    Subiendo la primera parte del código practica 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const url = 'https://api.datos.gob.mx/v1/condiciones-atmosfericas';
+  const estadosMX =[
+    {'id':1, 'name':'Aguascalientes'},
+    {'id':2, 'name':'Baja California'},
+    {'id':19, 'name':'Nuevo León'},
+    {'id':20, 'name':'Oaxaca'},
+    {'id':21, 'name':'Puebla'},
+    {'id':31, 'name':'Yucatán'},
+    {'id':32, 'name':'Zacatecas'}
+  ];
+  const [datos,setDatos] = useState([]);
+  const [estadoActual,setEstadoActual] = useState('Quintana Roo');
+
+  const consultarDatos = () =>{
+    return fetch(url)
+      .then((res) => res.json())
+      .then((condicionAtm) => setDatos(condicionAtm.results))
+  }
+  useEffect(() => {
+    consultarDatos();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <select onChange={(e) => setEstadoActual(e.target.value)}
+     >
+      <option value=''>Selecciona una opción</option>
+      {estadosMX.map((opcion) => (
+        <option  key={opcion.id} value={opcion.name}>
+          {opcion.name}
+        </option>
+      ))}
+     </select>
+     {estadoActual}
+     <h1>Estado del tiempo</h1>
+     {datos.map((ciudad,index) => {
+      return(
+        <div>
+        <p>{ciudad.name} - <i>{ciudad.skydescriptionlong}</i></p>
+        </div>
+      );
+     })}
+    </>
   );
 }
 
