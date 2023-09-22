aaf5e7422b5798fcd32cdf15ef7164f77674f322
Israel Boquin Cancino <ip345622@gmail.com>
2023-09-22 10:25:10 -0500

    Israel: Re organizar la estructura de archivos

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
-import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import { Navbar } from './componentes/Nabvar/Navbar';
+import Tiempo from './page/tiempo/tiempo';
 
 function App() {
-  const url = 'https://api.datos.gob.mx/v1/condiciones-atmosfericas?state=';
-  const estadosMX =[
-    {'id':1, 'name':'Aguascalientes'},
-    {'id':2, 'name':'Baja California'},
-    {'id':3, 'name':'Nuevo León'},
-    {'id':4, 'name':'Oaxaca'},
-    {'id':5, 'name':'Puebla'},
-    {'id':6, 'name':'Yucatan'},
-    {'id':7, 'name':'Zacatecas'},
-    {'id':8, 'name':'Chihuahua'}
-  ];
-  const [datos,setDatos] = useState([]);
-  const [estadoActual,setEstadoActual] = useState('Quintana Roo');
-  const [datosFiltrados, setDatosFiltrados] = useState([]);
-
-  const consultarDatos = () =>{
-    return fetch(url)
-      .then((res) => res.json())
-      .then((condicionAtm) => setDatos(condicionAtm.results))
-  }
-  useEffect(() => {
-    consultarDatos();
-  },[]);
-
-  const filtrarDatos = () => {
-    setDatosFiltrados(datos.filter((ciudad) => ciudad.state === estadoActual));
-  };
-
-  useEffect(() => {
-    filtrarDatos();
-  }, [estadoActual]);
 
   return (
-    <div className='Cuerpo'>
-     <select onChange={(e) => setEstadoActual(e.target.value)}
-     className='Select'>
-      <option  value='' className="option">Selecciona una opción</option>
-      {estadosMX.map((opcion) => (
-        <option  key={opcion.id} value={opcion.name}>
-          {opcion.name}
-        </option>
-      ))}
-</select>
-  <p className="estado-actual">{estadoActual}</p>
-  <h1 className='tiempo'>Estado del tiempo</h1>
-  {datosFiltrados.map((ciudad, index) => {
-    return (
-      <div>
-        <p className='estados'>{ciudad.name} - <i>{ciudad.skydescriptionlong}</i></p>
-      </div>
-    );
-  })}
+    <div>
+      <Navbar/>
+      <Tiempo />
     </div>
   );
 }
---
+++ src/page/tiempo/tiempo.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react';
+import '../../App.css';
+
+function Tiempo() {
+  const url = 'https://api.datos.gob.mx/v1/condiciones-atmosfericas?state=';
+  
+  const estadosMX =[
+    {'id':1, 'name':'Aguascalientes'},
+    {'id':2, 'name':'Baja California'},
+    {'id':3, 'name':'Nuevo Leon'},
+    {'id':4, 'name':'Oaxaca'},
+    {'id':5, 'name':'Puebla'},
+    {'id':6, 'name':'Yucatan'},
+    {'id':7, 'name':'Zacatecas'},
+    {'id':8, 'name':'Chihuahua'},
+    {'id':9, 'name':'Quintana Roo'}
+  ];
+  const [datos,setDatos] = useState([]);
+  const [estadoActual,setEstadoActual] = useState('Quintana Roo');
+  const [datosFiltrados, setDatosFiltrados] = useState([]);
+
+  const consultarDatos = () =>{
+    return fetch(url)
+      .then((res) => res.json())
+      .then((condicionAtm) => setDatos(condicionAtm.results))
+  }
+  useEffect(() => {
+    consultarDatos();
+  },[]);
+
+  const filtrarDatos = () => {
+    setDatosFiltrados(datos.filter((ciudad) => ciudad.state === estadoActual));
+  };
+
+  useEffect(() => {
+    filtrarDatos();
+  }, [estadoActual]);
+
+  return (
+    <div className='Cuerpo'>
+     <select onChange={(e) => setEstadoActual(e.target.value)}
+     className='Select'>
+      <option  value='' className="option">Selecciona una opción</option>
+      {estadosMX.map((opcion) => (
+        <option  key={opcion.id} value={opcion.name}>
+          {opcion.name}
+        </option>
+      ))}
+</select>
+  <p className="estado-actual">{estadoActual}</p>
+  <h1 className='tiempo'>Estado del tiempo</h1>
+  {datosFiltrados.map((ciudad, index) => {
+    return (
+      <div>
+        <p className='estados'>{ciudad.name} - <i>{ciudad.skydescriptionlong}</i></p>
+      </div>
+    );
+  })}
+    </div>
+  );
+}
+
+export default Tiempo;
